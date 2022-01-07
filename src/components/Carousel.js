a1fbe4a692e22dad7d579d36765e4f3bc233fc89
AEMuto <antoine.marseaud@gmail.com>
2022-01-07 10:10:28 +0100

    Début Carousel. Mise en forme CSS. Import des props pictures.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import chevronRight from '../images/chevron-carousel-right.svg';
+import chevronLeft from '../images/chevron-carousel-left.svg';
 
 class Carousel extends React.Component {
+  state = {
+    pictures: null,
+    current: 1
+  }
+
+  componentDidMount() {
+    const { pictures } = this.props;
+    this.setState({ pictures });
+  }
+
   render() {
+    const { pictures } = this.props;
+
     return (
       <div className="carousel">
-        <img src={this.props.pictures[0]} alt=""/>
+        {pictures.map((picture, index) => {
+          return <img src={picture} key={index}/>
+        })}
+        <div className="carousel__controls">
+          <button><img src={chevronLeft} alt="left arrow"/></button>
+          <button><img src={chevronRight} alt="right arrow"/></button>
+        </div>
       </div>
     )
   }
