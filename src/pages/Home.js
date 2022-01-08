3cad1c9b15ac04302b00a9093f92cdd422b6721a
AEMuto <antoine.marseaud@gmail.com>
2022-01-08 12:46:22 +0100

    Fetch des données depuis amazon S3, problème CORS. Upload du json sur github via ce commit.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,26 +8,50 @@ import LodgingCard from '../components/LodgingCard';
 import lodgings from '../lodgings';
 import backgroundHome from '../images/header-background-home.webp';
 
+const URL = '';
+
 class Home extends React.Component {
   state = {
+    data: null,
+    isLoading: false,
+    error: null,
     lodgings: lodgings,
   }
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    try {
+      const response = await fetch(URL);
+      console.log(response);
+      const result = await response.json();
+      this.setState({
+        data: JSON.parse(result),
+        isLoading: false,
+      });
+    } catch (error) {
+      this.setState({
+        error,
+        isLoading: false,
+      });
+    }
+  }
+
   render() {
     const headerTitle = 'Chez vous, partout et ailleurs';
     return (
       <Fragment>
-        <Nav />
+        <Nav/>
         <div className="wrapper">
           <Header title={headerTitle} image={backgroundHome}/>
           <section className="gallery">
             {this.state.lodgings.map(lodging => {
               return (
-                <LodgingCard key={lodging.id} data={lodging} />
+                <LodgingCard key={lodging.id} data={lodging}/>
               )
             })}
           </section>
         </div>
-        <Footer />
+        <Footer/>
       </Fragment>
     )
   }
